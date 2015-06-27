2ae6f2326ce440da0c44d71864a96fa2af622801
Kenan SUN <kenansun0@gmail.com>
2015-06-27 20:20:49 +0800

    [2015/06/27] Rewrite GameMap.buf part, now it's private. Full line related methods done

--- src/pytetris.py
+++ src/pytetris.py
@@ -91,27 +91,62 @@ class Block(object):
             self.pic = BlockTool.rotate_pic(self.pic)
 
 class GameMap(object):
+    NOTHING = 0
+    WALLS   = 1
+    BLOCKS  = 2
     def __init__(self):
         self.x_min = 0
         self.x_max = 10
         self.y_min = 0
         self.y_max = 30
-        self.point_buf = []
-        self.buf = [[1] + [0] * (self.y_max - self.y_min + 1 - 2) + [1] for i in range(self.x_max - self.x_min + 1 - 2)]
-        self.buf.insert(0, [1] * (self.y_max - self.y_min + 1))
-        self.buf.append([1] * (self.y_max - self.y_min + 1))
-        self.buf = tuple(self.buf)
-    def full_line(self):
-        pass
+        self.__row_count = self.y_max - self.y_min + 1
+        self.__col_count = self.x_max - self.x_min + 1
+        #self.point_buf = []
+        self.__buf = []
+        self.__init_buf()
+    def is_full_line(self, line_num):
+        if self.__buf[line_num][1: -1].count(2) == self.__col_count - 2:
+            return True
+        return False
+    def full_lines(self):
+        return (row for row in range(self.__row_count) if self.is_full_line(row))
+    def has_full_line(self):
+        for row in range(self.__row_count):
+            if self.is_full_line(row):
+                return True
+        return False
+    def set_line(self, line_num):
+        for col in range(1, self.__col_count - 1):
+            self.__buf[line_num][col] = 2
+    def clear_line(self, line_num):
+        for col in range(1, self.__col_count - 1):
+            self.__buf[line_num][col] = 0
     def clear_full_lines(self):
-        pass
+        for line_num in self.full_lines():
+            self.clear_line(line_num)
     def attach_block(self, b):
         self.__attach_points(b.pic, b.x, b.y)
+    def print_buf(self):
+        for line in self.__buf:
+            print line
+    def buf_at_col(self, col):
+        return [line[col] for line in self.__buf]
+    def buf_at_row(self, row):
+        return self.__buf[row]
+    def buf(self, x, y):
+        return self.__buf[y][x]
+    def buf_size(self):
+        return self.__col_count, self.__row_count  # x_max, y_max
     def __attach_points(self, pic, x, y):
         for point in pic:
-            self.buf[point[0] + x][point[1] + y] = 2
+            self.__buf[point[1] + y][point[0] + x] = 2  # buf[y][x]
             # self.point_buf.append([point[0] + x, point[1] + y])
         #self.point_buf.sort(key = lambda(point): point[1])
+    def __init_buf(self):
+        self.__buf = [[1] + [0] * (self.__col_count - 2) + [1] for i in range(self.__row_count - 2)]
+        self.__buf.insert(0, [1] * (self.__col_count))
+        self.__buf.append([1] * (self.__col_count))
+        self.test_buf = self.__buf
 
 class GamePaint(object):
     def __init__(self, handler = drawhandler.ConsolePaintHandler()):
@@ -122,10 +157,11 @@ class GamePaint(object):
     def draw_block(self, b):
         self.handler.draw_points(b.pic, b.x, b.y)
     def draw_map(self, m):
-        for i in range(len(m.buf)):
-            for j in range(len(m.buf[i])):
-                if m.buf[i][j] is not 0:
-                    self.handler.draw_point([i, j])
+        x_max, y_max = m.buf_size()
+        for x in range(x_max):
+            for y in range(y_max): 
+                if m.buf(x, y) is not 0:
+                    self.handler.draw_point([x, y])
         # # p11        p12
         # #
         # # p22        p21
@@ -231,7 +267,7 @@ def main():
         b = Block(i)
         b.x = 5
         b.y = 20
-        threading.Thread(target = key_thread, args = (b,)).start()
+        #threading.Thread(target = key_thread, args = (b,)).start()
         while True:
             # game_paint.repaint()
             # game_paint.draw_map(m)
@@ -245,35 +281,32 @@ def main():
             game_paint.draw_block(b)
             game_paint.paint()
             # print m.point_buf
-            time.sleep(0.05)
-            # if at_bottom(b, m):
-            #     m.attach_block(b)
-            #     # for line in m.buf:
-            #     #     print line
-            #     # raw_input()
-            #     break
-            # else:
-            #     b.drop()
+            time.sleep(0.5)
+            if at_bottom(b, m):
+                m.attach_block(b)
+                break
+            else:
+                b.drop()
 
 def at_bottom(block, m):
     for point in block.pic:
-        if m.buf[block.x + point[0]][block.y + point[1] - 1] is not 0:
+        if m.buf(block.x + point[0], block.y + point[1] - 1) is not 0:
             return True
     return False
 def at_left(block, m):
     for point in block.pic:
-        if m.buf[block.x + point[0] - 1][block.y + point[1]] is not 0:
+        if m.buf(block.x + point[0] - 1, block.y + point[1]) is not 0:
             return True
     return False
 def at_right(block, m):
     for point in block.pic:
-        if m.buf[block.x + point[0] + 1][block.y + point[1]] is not 0:
+        if m.buf(block.x + point[0] + 1, block.y + point[1]) is not 0:
             return True
     return False
 def can_rotate(block, m):
     rotated_pic = BlockTool.rotate_pic(block.pic)
     for point in block.rotated_pic:
-        if m.buf[block.x + point[0]][block.y + point[1]] is not 0:
+        if m.buf(block.x + point[0], block.y + point[1]) is not 0:
             return False
     return True
 
